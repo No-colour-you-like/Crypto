5eb42bb3cf4de5751587102462a4cf82103e05b8
Nikita <no.colour.you.like@gmail.com>
2021-05-08 20:38:31 +0300

    crypto process

--- js/script.js
+++ js/script.js
@@ -6,27 +6,80 @@ const dailyValue = document.querySelector('#daily-value'),
   dollarBtn = document.querySelector('#dollar-btn'),
   euroBtn = document.querySelector('#euro-btn'),
   sterlingBtn = document.querySelector('#sterling-btn'),
-  rubleBtn = document.querySelector('#ruble-btn');
+  rubleBtn = document.querySelector('#ruble-btn'),
+  dailyCryptoImg = document.querySelector('#total-crypto-img'),
+  cryptoBtnBTC = document.querySelector('#crypto-btn-btc'),
+  cryptoBtnETH = document.querySelector('#crypto-btn-eth'),
+  cryptoBtnXRP = document.querySelector('#crypto-btn-xrp'),
+  cryptoDifferenceValue = document.querySelectorAll('.cryptos__difference-value'),
+  cryptoDifferenceArrow = document.querySelectorAll('.cryptos__difference-arrow');
 
 
-const changeDayInfo = (data, currencyIcon) => {
-  dailyValue.textContent = data[data.length - 1].close.toLocaleString('ru') + `${currencyIcon}`;
-  dailyMax.textContent = data[data.length - 1].high.toLocaleString('ru');
-  dailyMin.textContent = data[data.length - 1].low.toLocaleString('ru');
-};
-
-const fetchInfo = async (cryptocurrency, currency, limit, currencyIcon) => {
-  let crypto = await fetch(`https://min-api.cryptocompare.com/data/v2/histoday?fsym=${cryptocurrency}&tsym=${currency}&limit=${limit}`);
-  let resp = await crypto.json();
+// Fetch API data
+const fetchInfo = async (cryptocurrency, currency, currencyIcon, limit = 1) => {
+  const crypto = await fetch(`https://min-api.cryptocompare.com/data/v2/histoday?fsym=${cryptocurrency}&tsym=${currency}&limit=${limit}`);
+  const resp = await crypto.json();
   const cryptoDailyData = resp.Data.Data;
 
-  changeDayInfo(cryptoDailyData, currencyIcon);
+  let dataClose = cryptoDailyData[cryptoDailyData.length - 1].close,
+    dataHigh = cryptoDailyData[cryptoDailyData.length - 1].high,
+    dataLow = cryptoDailyData[cryptoDailyData.length - 1].low;
+
+
+  dailyValue.textContent = dataClose.toLocaleString('ru') + ` ${currencyIcon}`;
+  dailyMax.textContent = dataHigh.toLocaleString('ru');
+  dailyMin.textContent = dataLow.toLocaleString('ru');
 };
 
-fetchInfo('BTC', 'USD', '30', '$');
+fetchInfo('BTC', 'USD', '$');
+
+const fetchDiffrenceInfo = async (cryptocurrency, currency, limit = 1) => {
+  const crypto = await fetch(`https://min-api.cryptocompare.com/data/v2/histoday?fsym=${cryptocurrency}&tsym=${currency}&limit=${limit}`);
+  const resp = await crypto.json();
+  const cryptoDailyData = resp.Data.Data;
+
+  let dataOpen = cryptoDailyData[cryptoDailyData.length - 1].open,
+    dataClose = cryptoDailyData[cryptoDailyData.length - 1].close;
+  if (cryptocurrency === 'BTC') {
+    cryptoDifferenceValue[0].textContent = (100 - (dataOpen * 100 / dataClose)).toFixed(2) + ' %';
+  } else if (cryptocurrency === 'ETH') {
+    cryptoDifferenceValue[1].textContent = (100 - (dataOpen * 100 / dataClose)).toFixed(2) + ' %';
+  } else {
+    cryptoDifferenceValue[2].textContent = (100 - (dataOpen * 100 / dataClose)).toFixed(2) + ' %';
+  }
+
+};
+
+fetchDiffrenceInfo('BTC', 'USD')
+fetchDiffrenceInfo('ETH', 'USD')
+fetchDiffrenceInfo('XRP', 'USD')
+
+// Change daily main currency
+const changeDailyCurrency = (currency, currencySign) => {
+  if (dailyValue.classList.contains('btc')) {
+    fetchInfo('BTC', currency, currencySign);
+  } else if (dailyValue.classList.contains('eth')) {
+    fetchInfo('ETH', currency, currencySign);
+  } else {
+    fetchInfo('XRP', currency, currencySign);
+  }
+};
+
+// Set main cryptocurrency 
+const setCryptocurrency = (cryptoImg, cryptoName) => {
+  dailyCryptoImg.setAttribute('src', cryptoImg);
+  dailyValue.className = `overview__total ${cryptoName.toLowerCase()}`;
+  fetchInfo(cryptoName, 'USD', '$');
+};
 
 
-dollarBtn.addEventListener('click', () => fetchInfo('BTC', 'USD', '30', '$'));
-euroBtn.addEventListener('click', () => fetchInfo('BTC', 'EUR', '30', '€'));
-sterlingBtn.addEventListener('click', () => fetchInfo('BTC', 'GBP', '30', '£'));
-rubleBtn.addEventListener('click', () => fetchInfo('BTC', 'RUB', '30', '₽'));
+// Change daily currency info on click
+dollarBtn.addEventListener('click', () => changeDailyCurrency('USD', '$'));
+euroBtn.addEventListener('click', () => changeDailyCurrency('EUR', '€'));
+sterlingBtn.addEventListener('click', () => changeDailyCurrency('GBP', '£'));
+rubleBtn.addEventListener('click', () => changeDailyCurrency('RUB', '₽'));
+
+// Set main cryptocurrency on click
+cryptoBtnBTC.addEventListener('click', () => setCryptocurrency('img/bitcoin.svg', 'BTC'));
+cryptoBtnETH.addEventListener('click', () => setCryptocurrency('img/ethereum.svg', 'ETH'));
+cryptoBtnXRP.addEventListener('click', () => setCryptocurrency('img/ripple.svg', 'XRP'));
