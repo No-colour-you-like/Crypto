2b43a828cb1fe6b6d6a048d3740dc740bc72cc80
Nikita <no.colour.you.like@gmail.com>
2021-05-09 13:33:35 +0300

    exchange complete

--- js/script.js
+++ js/script.js
@@ -12,7 +12,12 @@ const dailyValue = document.querySelector('#daily-value'),
   cryptoBtnETH = document.querySelector('#crypto-btn-eth'),
   cryptoBtnXRP = document.querySelector('#crypto-btn-xrp'),
   cryptoDifferenceValue = document.querySelectorAll('.cryptos__difference-value'),
-  cryptoDifferenceArrow = document.querySelectorAll('.cryptos__difference-arrow');
+  cryptoDifferenceArrow = document.querySelectorAll('.cryptos__arrow'),
+  exchangeSelectFirst = document.querySelector('#exchange-select-first'),
+  exchangeSelectSecond = document.querySelector('#exchange-select-second'),
+  exchangeInputFirst = document.querySelector('#exchange-input-first'),
+  exchangeInputSecond = document.querySelector('#exchange-input-second'),
+  exchangeBtn = document.querySelector('#exchange-btn');
 
 
 // Fetch API data
@@ -25,7 +30,6 @@ const fetchInfo = async (cryptocurrency, currency, currencyIcon, limit = 1) => {
     dataHigh = cryptoDailyData[cryptoDailyData.length - 1].high,
     dataLow = cryptoDailyData[cryptoDailyData.length - 1].low;
 
-
   dailyValue.textContent = dataClose.toLocaleString('ru') + ` ${currencyIcon}`;
   dailyMax.textContent = dataHigh.toLocaleString('ru');
   dailyMin.textContent = dataLow.toLocaleString('ru');
@@ -33,6 +37,7 @@ const fetchInfo = async (cryptocurrency, currency, currencyIcon, limit = 1) => {
 
 fetchInfo('BTC', 'USD', '$');
 
+// Fetch and set cryptocurrency day difference
 const fetchDiffrenceInfo = async (cryptocurrency, currency, limit = 1) => {
   const crypto = await fetch(`https://min-api.cryptocompare.com/data/v2/histoday?fsym=${cryptocurrency}&tsym=${currency}&limit=${limit}`);
   const resp = await crypto.json();
@@ -40,19 +45,42 @@ const fetchDiffrenceInfo = async (cryptocurrency, currency, limit = 1) => {
 
   let dataOpen = cryptoDailyData[cryptoDailyData.length - 1].open,
     dataClose = cryptoDailyData[cryptoDailyData.length - 1].close;
-  if (cryptocurrency === 'BTC') {
-    cryptoDifferenceValue[0].textContent = (100 - (dataOpen * 100 / dataClose)).toFixed(2) + ' %';
-  } else if (cryptocurrency === 'ETH') {
-    cryptoDifferenceValue[1].textContent = (100 - (dataOpen * 100 / dataClose)).toFixed(2) + ' %';
-  } else {
-    cryptoDifferenceValue[2].textContent = (100 - (dataOpen * 100 / dataClose)).toFixed(2) + ' %';
-  }
 
+  const calculateDifference = (100 - (dataOpen * 100 / dataClose)).toFixed(2);
+
+  const setDifference = (cryptocurrencyIndex) => {
+    cryptoDifferenceValue[cryptocurrencyIndex].textContent = calculateDifference + ' %';
+
+    if (calculateDifference >= 0) {
+      cryptoDifferenceArrow[cryptocurrencyIndex].setAttribute('src', 'img/arrow-up.svg');
+    } else {
+      cryptoDifferenceArrow[cryptocurrencyIndex].setAttribute('src', 'img/arrow-down.svg');
+    }
+  };
+
+  if (cryptocurrency === 'BTC') {
+    setDifference(0);
+  } else if (cryptocurrency === 'ETH') {
+    setDifference(1);
+  } else {
+    setDifference(2);
+  }
 };
 
-fetchDiffrenceInfo('BTC', 'USD')
-fetchDiffrenceInfo('ETH', 'USD')
-fetchDiffrenceInfo('XRP', 'USD')
+// Update all cryptocurrency defferences
+const startCheckDifference = (currency) => {
+  fetchDiffrenceInfo('BTC', currency);
+  fetchDiffrenceInfo('ETH', currency);
+  fetchDiffrenceInfo('XRP', currency);
+};
+
+startCheckDifference('USD');
+
+// Update cryptocurrency difference every 30 sec
+setInterval(() => {
+  startCheckDifference('USD');
+}, 30000);
+
 
 // Change daily main currency
 const changeDailyCurrency = (currency, currencySign) => {
@@ -83,3 +111,40 @@ rubleBtn.addEventListener('click', () => changeDailyCurrency('RUB', '₽'));
 cryptoBtnBTC.addEventListener('click', () => setCryptocurrency('img/bitcoin.svg', 'BTC'));
 cryptoBtnETH.addEventListener('click', () => setCryptocurrency('img/ethereum.svg', 'ETH'));
 cryptoBtnXRP.addEventListener('click', () => setCryptocurrency('img/ripple.svg', 'XRP'));
+
+
+// Fetch and calculate currencies
+const fetchCurrencies = async (cryptoCurrency, currency) => {
+  const currencies = await fetch(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${cryptoCurrency}&tsyms=${currency}`);
+  const resp = await currencies.json();
+
+  let currencyValue = resp[cryptoCurrency][currency];
+
+  exchangeInputSecond.textContent = (exchangeInputFirst.value * currencyValue).toLocaleString('ru');
+};
+
+//Calculate currencies 
+exchangeInputFirst.addEventListener('input', () => fetchCurrencies(exchangeSelectFirst.value, exchangeSelectSecond.value));
+
+exchangeSelectFirst.addEventListener('change', () => fetchCurrencies(exchangeSelectFirst.value, exchangeSelectSecond.value))
+
+exchangeSelectSecond.addEventListener('change', () => fetchCurrencies(exchangeSelectFirst.value, exchangeSelectSecond.value));
+
+
+//Swap currencies 
+exchangeBtn.addEventListener('click', () => {
+  let firstOptions = exchangeSelectFirst.innerHTML,
+    secondOptions = exchangeSelectSecond.innerHTML,
+    selectFirstValue = exchangeSelectFirst.value,
+    selectSecondValue = exchangeSelectSecond.value;
+
+  let optionsArr = [firstOptions, secondOptions];
+
+  exchangeSelectFirst.innerHTML = optionsArr[1];
+  exchangeSelectSecond.innerHTML = optionsArr[0];
+
+  exchangeSelectSecond.value = selectFirstValue;
+  exchangeSelectFirst.value = selectSecondValue;
+
+  fetchCurrencies(exchangeSelectFirst.value, exchangeSelectSecond.value);
+});
